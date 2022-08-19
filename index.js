724a69ff0d84593e315c41308da35d34ce132cbd
Amina Hayat <hayat.amina@hotmail.com>
2022-08-19 16:50:39 +0100

    js

--- index.js
+++ index.js
@@ -7,3 +7,40 @@ const Intern = require("./lib/Intern");
 
 const renderhtml = require("./src/temp");
 const members = [];
+
+function promptManager() {
+    inquirer
+      .prompt([
+        {
+    type: 'input',
+    name: 'managername',
+    message: 'What is the managers name?'
+}, {
+    type: 'input',
+    name: 'managerid',
+    message: 'Enter your ID number'
+}, {
+    type: 'input',
+    name: 'manageremail',
+    message: 'Enter your email'
+}, {
+    type: 'input',
+    name: 'managerofficenumber',
+    message: 'Enter your office number'
+},
+
+])
+.then((answers) => {
+
+    const manager = new Manager(
+        answers.managername,
+        answers.managerid,
+        answers.manageremail,
+        answers.managerofficenumber
+      );
+
+      members.push(manager);
+
+      menu();
+    });
+    }
