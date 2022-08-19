fae28dc80749276a7145718e91cc59f76a62729a
Amina Hayat <hayat.amina@hotmail.com>
2022-08-19 16:56:35 +0100

    changes to js files

---
+++ test/inter.test.js
@@ -0,0 +1,27 @@
+const Intern = require("../lib/Intern")
+
+describe("Creates an Intern", () => {
+    it ("Intern name gets printed", () => {
+        const name = "Soso";
+        const Emp = new Intern(name);
+        expect(Emp.name).toBe(name);
+    })
+
+    it ("Intern id gets printed", () => {
+        const id = "3";
+        const Emp = new Intern("Soso", id);
+        expect(Emp.id).toBe(id);
+    })
+
+    it ("Intern email gets printed", () => {
+        const email = "Soso@gmail.com";
+        const Emp = new Intern("Soso", "3", email);
+        expect(Emp.email).toBe(email);
+    })
+
+    it ("Intern school gets printed", () => {
+        const school = "Hameds school";
+        const Emp = new Intern("Soso", "3", "Soso@gmail.com", school);
+        expect(Emp.getSchool()).toBe(school);
+    })
+})
--- test/manager.test.js
+++ test/manager.test.js
@@ -2,26 +2,26 @@ const Manager = require("../lib/Manager")
 
 describe("Creates a Manager", () => {
     it ("Manager name should get printed", () => {
-        const name = "Bob";
+        const name = "Amina";
         const Emp = new Manager(name);
         expect(Emp.name).toBe(name);
     })
 
     it ("Manager id gets printed", () => {
         const id = "4";
-        const Emp = new Manager("Bob", id);
+        const Emp = new Manager("Amina", id);
         expect(Emp.id).toBe(id);
     })
 
     it ("Manager email gets printed", () => {
-        const email = "bob@hotmail.co.uk";
-        const Emp = new Manager("Bob", "4", email);
+        const email = "Amina@hotmail.co.uk";
+        const Emp = new Manager("Amina", "4", email);
         expect(Emp.email).toBe(email);
     })
 
     it ("Manager number gets printed", () => {
         const officeNumber = "12";
-        const Emp = new Manager("Bob", "4", "bob@hotmail.co.uk", officeNumber);
+        const Emp = new Manager("Amina", "4", "Amina@hotmail.co.uk", officeNumber);
         expect(Emp.getOfficeNumber()).toBe(officeNumber);
     })
 })
