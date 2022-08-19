dc3ff28fa0eb58df9d083c4d07f701d6c97c3893
Amina Hayat <hayat.amina@hotmail.com>
2022-08-19 16:39:16 +0100

    created roue

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require("fs");
 
-const Manager = require("");
+const Manager = require("./lib/manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
